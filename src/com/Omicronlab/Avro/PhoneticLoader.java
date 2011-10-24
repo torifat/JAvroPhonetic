6305e4f1cf6190d3876ab9383e8e167f32daae01
Rifat Nabi <to.rifat@gmail.com>
2011-10-25 01:56:52 +0600

    clean, base ready

--- src/com/Omicronlab/Avro/PhoneticLoader.java
+++
@@ -1,9 +0,0 @@
-package com.omicronlab.avro;
-
-import java.util.List;
-
-import com.omicronlab.avro.phonetic.Pattern;
-
-public interface PhoneticLoader {
-	public List<Pattern> getPatterns() throws Exception;
-}
